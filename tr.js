a729a4154f23151c53be1e3b3c104f89bd699652
bucksh0t <67687653+inelity@users.noreply.github.com>
2023-06-29 21:11:13 +0300

    Create tr.js

---
+++ tr.js
@@ -0,0 +1,403 @@
+module.exports = {
+    // home top Card
+    "card_home_title": "Başlarken",
+    "card_home_desc": "Düşük Ücretle Büyük Hizmet! Hemen Ürününü Satın Al Ve Sende %var% Hizmetine Var!",
+    "card_panel_go": "Panel' e Git!",
+    "card_login_go": "Giriş Yap!",
+    "card_contact_go": "Telegram İletişim",
+
+    // best selling products
+    "mostsell_title": "En Çok Satan Ürünler;",
+    "mostsell_popular": "POPÜLER",
+    "mostsell_examine": "İncele",
+    "mostsell_condition": "Gizlilik Geçerlidir. Tüm Hakları Gizlidir.",
+
+    // those who prefer us
+    "preferus_title": "Bizi Tercih Edenler",
+    "preferus_desc": "Biz kaliteyi ve ucuz' u yakalamak isteyen müşterilerimize hizmet sağlarız. Hizmet felsefemiz her zaman bu olmuştur. Bizi tercih edenleri KALITE Bekliyor. Haydi siparişini ver!",
+    "preferus_users": "Kullanıcılar",
+    "preferus_customer": "Müşteri",
+
+    // our team
+    "team_title": "Takımımız",
+    "team_desc": "Projenin Devamlılığını Sağlayan Projede Emeği Geçen Kişiler!",
+
+    // products
+    "products_condition": "Gizlilik Geçerlidir. Tüm Hakları Gizlidir.",
+    "products_notfound": "Ürün Bulunamadı",
+
+    // Password Reset
+    "changepass_title": "Şifre Değiştir",
+    "changepass_noacc": "Hesabım Yok",
+    "changepass_reg": "Kayıt Ol",
+    "changepass_wait_title": "Şifre Güncelleniyor!",
+    "changepass_wait_desc": "Lütfen Bekleyin İşlem Yapılıyor..",
+
+    // Password Reset Request
+
+    "saveacc_title": "Hesabını Kurtar",
+    "saveacc_sendmail": "E-Mail Gönder",
+    "saveacc_noacc": "Hesabım Yok",
+    "saveacc_alert_title": "E-Mail Kontrol Edin!",
+    "saveacc_alert_desc": "E-Mail / Kullanıcı Adı Bağlı Olduğu Mail Hesabınızı İnceleyin (Hesap Yoksa Gönderilemez!)",
+
+    // Login
+    "login_title": "Giriş Yap!",
+    "login_button": "Giriş Yap!",
+    "login_wait_title": "Giriş Yapılıyor!",
+    "login_wait_desc": "Lütfen Bekleyin Bilgiler Kontrol Ediliyor..",
+
+    // Register
+    "register_title": "Hesap Oluştur",
+    "register_button": "Hesap Oluştur!",
+    "register_noacc": "Hesabım Yok",
+    "register_wait_title": "Kayıt Oluşturuluyor!",
+    "register_wait_desc": "Lütfen Bekleyin Kaydınızı Oluşturuyoruz..",
+
+    // Buy
+    "buy_footer": "Hiçbir Koşulda İade & İptal Sağlanmaz!",
+    "buy_watch": "Video İzle",
+    "buy_block_title": "Ürün Satın Alınamaz",
+    "buy_block_desc": "Yönetici Tarafından Bu Ürünün Satın Alınması Yasaklanmıştır!",
+    "buy_question_title": "Lütfen Doldurun",
+    "buy_question_footer": "Lütfen Doğru Bilgiler Girdiğinizden Emin Olun! Hatalı Girilirse İptal Edilir / İade Geçersizdir!",
+    "buy_wait_title": "Ödeme Yapılıyor..",
+    "buy_wait_desc": "Lütfen Bekleyin..",
+    "buy_nomoney_title": "Lütfen Bakiye Ekleyin!",
+    "buy_nomoney_desc": "Bu Ürünü Almak İçin Yeterli Miktarda Bakiyeniz Bulunmamaktadır",
+    "buy_nomoney_button": "Hızlı Bakiye Ekle",
+    "buy_member_title": "Botu Ekleyin!",
+    "buy_member_desc": "Lütfen Sunucunuzuza Üye Gönderebilmemiz İçin Botu Ekleyin!",
+    "buy_member_button": "Botu Ekle",
+    "buy_member_footer": "İşlem Tamamlanana Kadar Botu Sunucudan Atmayınız!",
+
+
+    // Dashboard Tickets
+    "dashboard_tickets_title": "Destek Talebi",
+    "dashboard_tickets_replied": "Yanıtlandı",
+    "dashboard_tickets_closed": "Kapatıldı",
+    "dashboard_tickets_notfound": "Hiç Ticket Oluşturmadınız",
+    "dashboard_tickets_wait": "Cevap Bekliyor",
+
+    // Dashboard Ticket
+    "dashboard_ticket_title": "Destek Talebi",
+    "dashboard_ticket_continuetitle": "Başlıklı Destek Talebi",
+    "dashboard_ticket_footer": "Gönderdiğiniz Her Mesajdan Hesabınız Sorumludur.",
+    "dashboard_ticket_wait_title": "İşlem Yapılıyor!",
+    "dashboard_ticket_wait_desc": "Lütfen Bekleyin İşlem Devam Ediyor..",
+
+    // Dashboard Ticket Create
+    "dashboard_ticket_create_title": "Destek Talebi Oluştur",
+    "dashboard_ticket_create_placeholder": "Sorununuzu Belirtin..",
+    "dashboard_ticket_create_button": "Oluştur",
+    "dashboard_ticket_create_wait_title": "İşlem Yapılıyor!",
+    "dashboard_ticket_create_wait_desc": "Lütfen Bekleyin İşlem Devam Ediyor..",
+
+    // Dashboard Settings
+    "dashboard_settings_title": "Ayarlar",
+    "dashboard_settings_present": "Mevcut Şifre",
+    "dashboard_settings_newpass": "Yeni Şifre",
+    "dashboard_settings_repass": "Tekrar Şifre",
+    "dashboard_settings_button": "Şire Değiştir",
+    "dashboard_settings_wait_title": "İşlem Yapılıyor!",
+    "dashboard_settings_wait_desc": "Lütfen Bekleyin İşlem Devam Ediyor..",
+
+    // Dashboard Orders
+    "dashboard_orders_title": "Ürünlerim",
+    "dashboard_orders_id": "Id (For Real Id Click)",
+    "dashboard_orders_request": "Teslim Aşamasında!",
+    "dashboard_orders_accept": "Teslim Edildi!",
+    "dashboard_orders_reject": "İade Edildi!",
+    "dashboard_orders_download": "Teslim Edildi, Hemen İndir",
+    "dashboard_orders_copy_title": "Başarıyla, Id Kopyalandı",
+    "dashboard_orders_copy_desc": "Id Başarıyla Önbelleğine Kopyalandı!",
+
+    // Dashboard Add Money
+    "dashboard_addmoney_title": "Bakiye Yükle",
+    "dashboard_addmoney_type": "Ödeme Türü",
+    "dashboard_addmoney_footer": "Bakiye İade Edilemez, Aktarılamaz.",
+    "dashboard_addmoney_button": "Yükle",
+    "dashboard_addmoney_wait_title": "İşlem Yapılıyor!",
+    "dashboard_addmoney_wait_desc": "Lütfen Bekleyin İşlem Devam Ediyor..",
+    "dashboard_addmoney_redirect_title": "Ödeme Yönlendiriliyor..",
+    "dashboard_addmoney_redirect_desc": "Ödeme Servis' ine Yönlendiriliyorsunuz. Sitemiz Bilgilerinize Erişemez!",
+
+    // Dashboard Main
+    "dashboard_main_title": "Panel",
+    "dashboard_main_card1": "Alışveriş",
+    "dashboard_main_card2": "Bakiye",
+    "dashboard_main_card3": "Yüklenen Para",
+    "dashboard_main_history_title": "Ödeme Geçmişi",
+
+    // Page Badge
+    "page_badge_balance": "Bakiye",
+    "page_badge_welcome": "Selam, %var%",
+
+    // Page footer
+    "page_footer_slogan": "Dijital Ürün Alışveriş Platform",
+    "page_footer_gopanel": "Panel' e Git",
+    "page_footer_gologin": "Giriş Yap",
+
+    // Page Header
+    "page_header_gologin": "Giriş Yap",
+    "page_header_gopanel": "Panel' e Git",
+    "page_header_home": "Ana Sayfa",
+    "page_header_allproducts": "Tüm Ürünler",
+
+    // Page Sidebar
+    "page_sidebar_user_panel": "Panel",
+    "page_sidebar_user_orders": "Siparişlerim",
+    "page_sidebar_user_ticket": "Destek Talebi",
+    "page_sidebar_user_product": "Ürünler",
+    "page_sidebar_user_addmoney": "Bakiye Yükle",
+    "page_sidebar_user_settings": "Ayarlar",
+    "page_sidebar_user_logout": "Çıkış Yap",
+
+    // Metas
+    "meta_title_home": "Ana Sayfa",
+    "meta_desc_home": "Düşük ücretle En iyi Hizmet Ve Kalite garantili Platform, Sende aramıza katıl ve kalitenin bir parçası ol!",
+    "meta_title_products": "Ürün Satın Al",
+    "meta_title_buy": "Ürün Satın Al",
+    "meta_title_register": "Kayıt Ol",
+    "meta_title_login": "Giriş Yap",
+    "meta_title_forgetpass": "Şifremi Unuttum",
+    "meta_title_resetpass": "Şifre Değiştir",
+    "meta_title_error": "Hata",
+    "meta_title_money": "Sanal Pos",
+    
+    "meta_title_tickets": "Destek Taleplerim",
+    "meta_title_ticketcreate": "Destek Talebi Oluştur",
+    "meta_title_ticketread": "Destek Talebi",
+    "meta_title_orders": "Siparişlerim",
+    "meta_title_settings": "Ayarlar",
+    "meta_title_addmoney": "Bakiye Yükle",
+    "meta_title_dashboard": "Panel",
+
+    /* Api */
+
+    // Api Meta
+    "api_meta_shipy_success_title": "Başarıyla, Ödeme Tamamlandı",
+    "api_meta_shipy_success_desc": "Lütfen Üyelik Panelinizi İnceleyin!",
+    "api_meta_shipy_error_title": "Ödeme Yapılırken Hata Oluştu",
+    "api_meta_shipy_error_desc": "Lütfen Tekrar Deneyin.",
+    "api_meta_products": "Satışta Olan %var% Ürünlerimiz. Hemen Satın Al!",
+    "api_meta_register": "Hemen Sende Hesap Oluştur Alışverişe Başla!",
+    "api_meta_login": "Hemen Hesabına Giriş Yap Kaldığın Yerden Devam Et! Her Zaman En Kaliteli Satış!",
+    "api_meta_forgetpass": "Şifreni mi Unuttun? Sorun Yok Her Zaman Arkandayız. Burda Hesaplar Yok Olmaz!",
+
+
+    // Api Register
+    "api_register_code_notfound_title": "Email Doğrulama!",
+    "api_register_code_notfound_desc": "Kod Bulunamadı!",
+    "api_register_code_already_title": "Email Doğrulama!",
+    "api_register_code_already_desc": "Email Zaten Doğrulandı!",
+    "api_register_code_sucess_title": "Başarıyla Doğrulandı!",
+    "api_register_code_sucess_desc": "Email Başarıyla Doğrulandı, Yönlendiriliyorsunuz..",
+
+    // Api Forget Password
+    "api_forgetpass_notfound_title": "Geçersiz Kimlik",
+    "api_forgetpass_notfound_desc": "Bağlantı Artık Yok Veya Hiç Var Olmadı!",
+
+
+    // Api Product
+    "api_product_notfound": "Ürün Bulunamadı!",
+    "api_order_notfound": "Product Deleted!",
+
+    // Api Ticket Create
+    "api_ticketcreate_title_error_title": "Başlık Uzun Veya Kısa!",
+    "api_ticketcreate_title_error_desc": "Lütfen Yeterli Seviyede Bir Başlık Girin!",
+    "api_ticketcreate_category_error_title": "Kategori Bulunamadı",
+    "api_ticketcreate_category_error_desc": "Lütfen Uygun Kategori Kullanın!",
+    "api_ticketcreate_message_error_title": "Mesaj Uzun Veya Kısa!",
+    "api_ticketcreate_message_error_desc": "Lütfen Yeterli Seviyede Bir Mesaj Girin!",
+    "api_ticketcreate_replyreq_title": "Daha Fazla Ticket Açılamaz",
+    "api_ticketcreate_replyreq_desc": "Lütfen Sabırlı Olunuz..",
+    "api_ticketcreate_success_title": "Başarıyla, Ticket Oluşturuldu",
+    "api_ticketcreate_success_desc": "Lütfen Cevaplanmasını Bekleyiniz..",
+
+
+    // Api Ticket Send
+    "api_ticketsend_ticket_notfound_title": "Ticket Bulunamadı",
+    "api_ticketsend_ticket_notfound_desc": "Lütfen Yöneticiye Ulaşın!",
+    "api_ticketsend_message_error_title": "Mesaj Uzun Veya Kısa!",
+    "api_ticketsend_message_error_desc": "Lütfen Yeterli Seviyede Bir Mesaj Girin!",
+    "api_ticketsend_replyreq_title": "Lütfen Mesajın Cevaplanmasını Bekleyin.",
+    "api_ticketsend_replyreq_desc": "Üst üste Mesaj Atılamaz!",
+    "api_ticketsend_success_title": "Başarıyla, Mesaj Gönderildi",
+    "api_ticketsend_success_desc": "Lütfen Cevaplanmasını Bekleyiniz..",
+
+
+    // Api Buy
+    "api_buy_error_notfound_title": "Ürün Bulunamadı!",
+    "api_buy_error_notfound_desc": "Ürün Artık Mevcut Değil Veya Artık Yok!",
+    "api_buy_error_maxbuy_title": "Ürün Daha Fazla Alınamaz!",
+    "api_buy_error_maxbuy_desc": "Bu Ürün İçin Max Alım Miktarı %var% Olarak Belirlenmiştir.",
+    "api_buy_error_balance_title": "Lütfen Bakiye Ekleyin!",
+    "api_buy_error_balance_desc": "Bu Ürünü Almak İçin Yeterli Miktarda Bakiyeniz Bulunmamaktadır",
+    "api_buy_error_notmatch_title": "Bilgiler Yanlış!",
+    "api_buy_error_notmatch_desc": "Lütfen İstenilen Bilgileri Doğru Girin!",
+    "api_buy_success_title": "Başarıyla Ürün Satın Alındı!",
+    "api_buy_success_desc": "Üyeliğinize Ürün Eklenmiştir!",
+
+    // Sell
+    "api_buy_sell_apierror": "Api Bağlantısı Tamamlanamadı!",
+    "api_buy_control_error_invite_title": "Davet Doğrulama Hatası!",
+    "api_buy_control_error_invite_desc": "Davet Doğrulanamaz!",
+    "api_buy_control_error_server_title": "Sunucu Bulunamadı!",
+    "api_buy_control_error_server_desc": "Lütfen Daveti Doğru Girin!",
+    "api_buy_control_error_bot_title": "Lütfen Botumuzu Ekleyin!",
+    "api_buy_control_error_bot_desc": "Botu ekleyin!",
+    "api_buy_control_error_stock_title": "Stok Yok!",
+    "api_buy_control_error_stock_desc": "Yeterli Stoğumuz Yok Lütfen Daha Sonra Tekrar Deneyin!",
+    "api_buy_control_error_title": "Sorun Oluştu",
+    "api_buy_control_error_desc": "Sistem Bağlantısında Sorun Oluştu!",
+    "api_buy_control_error_maxmember_title": "Çok Fazla Üye!",
+    "api_buy_control_error_maxmember_desc": "Bu Sunucuya Toplam %var% Üye Gönderildi! Daha Fazla Gönderilemez!",
+    "api_buy_control_error_maxboost_title": "Çok Fazla Boost!",
+    "api_buy_control_error_maxboost_desc": "Bu Sunucuya Toplam %var% Boost Gönderildi! Daha Fazla Gönderilemez!",
+
+    // Api Add Money
+    "api_addmoney_error_amount_title": "Miktar Yanlış Girildi",
+    "api_addmoney_error_amount_desc": "Lütfen Sayı Biçiminde Girin!",
+    "api_addmoney_error_amount2_title": "Miktar Büyük Veya Küçük",
+    "api_addmoney_error_amount2_desc": "Lütfen Miktar 5-1000₺ Arasında Girin!",
+    "api_addmoney_error_noactive_title": "Ödeme Aktif Değil",
+    "api_addmoney_error_noactive_desc": "Ödeme Sistemi Yönetici Tarafından Kapatıldı!",
+    "api_addmoney_error_already_title": "İşlem Zaten Yapıldı!",
+    "api_addmoney_error_already_desc": "İşlem Daha Önce İşlendi!",
+
+
+    // Api Change Password
+    "api_changepass_error_password_desc": "Lütfen Hesap Şifrenizi Girin!",
+    "api_changepass_error_oldpassword_desc": "Lütfen Hesap Şifrenizi Girin!",
+    "api_changepass_error_newpassword_desc": "Lütfen Yeni Şifre Girin!",
+    "api_changepass_error_repassword_desc": "Lütfen Tekrar Şifre Girin!",
+    "api_changepass_error_norepassword_title": "Tekrar Yanlış!",
+    "api_changepass_error_norepassword_desc": "Lütfen Tekrar Şifre Doğru Girin!",
+    "api_changepass_error_entrynewrepassword_title": "Karakter Çok Veya Az!",
+    "api_changepass_error_entrynewrepassword_desc": "Lütfen Yeni Şifre Doğru Girin!",
+    "api_changepass_wrong_oldpassword_title": "Şifreniz Yanlış!",
+    "api_changepass_wrong_oldpassword_desc": "Lütfen Şifrenizi Doğru Girin!",
+    "api_changepass_wrong_reoldpassword_title": "Eski Şifre Tekrar Edilemez!",
+    "api_changepass_wrong_reoldpassword_desc": "Lütfen Yeni Şifrenizi Farklı Girin!",
+    "api_changepass_success_title": "Şifre Değiştirildi!",
+    "api_changepass_success_desc": "Şifre Başarıyla Değiştirildi!",
+
+
+    // Api Login
+    "api_login_error_already_title": "Zaten Giriş Yaptınız!",
+    "api_login_error_already_desc": "Sorun Oluştuğunu Düşünüyorsanız Bizimle İletişime Geçin!",
+    "api_login_error_email": "Lütfen Email / Kullanıcı Adı Girin!",
+    "api_login_error_password": "Lütfen Şifre Girin!",
+    "api_login_error_notfound_title": "Hesap Bulunamadı!",
+    "api_login_error_notfound_desc": "Email-Kullanıcı Adı Veya Şifreniz Yanlış!",
+    "api_login_error_verfreq_title": "Kaydını Tamamla!",
+    "api_login_error_verfreq_desc": "Email' inize Gelen Link İle Kaydını Tamamla!",
+    "api_login_success_title": "Başarıyla Giriş Yapıldı!",
+    "api_login_success_desc": "Başarıyla Giriş Yaptınız, Yönlendiriliyorsunuz..",
+
+
+    // Api Reset Password Change
+    "api_resetpass_error_password": "Lütfen Şifre Girin!",
+    "api_resetpass_error_repassword": "Lütfen Tekrar Şifre Girin!",
+    "api_resetpass_wrong_repassword_title": "Tekrar Yanlış!",
+    "api_resetpass_wrong_repassword_desc": "Lütfen Tekrar Şifre Doğru Girin!",
+    "api_resetpass_wrong_password_title": "Karakter Çok Veya Az!",
+    "api_resetpass_wrong_password_desc": "Lütfen Şifre Doğru Girin!",
+    "api_resetpass_error_notfound_title": "Geçersiz Key!",
+    "api_resetpass_error_notfound_desc": "Key Artık Geçersiz Veya Hiç Var Olmadı!",
+    "api_resetpass_error_retry_title": "Eski Şifre Tekrar Edilemez!",
+    "api_resetpass_error_retry_desc": "Lütfen Yeni Şifrenizi Farklı Girin!",
+    "api_resetpass_success_title": "Başarıyla Şifre Değiştirildi!",
+    "api_resetpass_success_desc": "Giriş Yap! Kaldığınız Yerden Devam Edin! Yönlendiriliyorsunuz..",
+
+
+    // Api Register
+    "api_register_error_email": "Lütfen Email Girin!",
+    "api_register_error_username": "Lütfen Kullanıcı Adı Girin!",
+    "api_register_error_password": "Lütfen Şifre Girin!",
+    "api_register_error_repassword": "Lütfen Tekrar Şifre Girin!",
+
+    "api_register_wrong_utfusername_title": "Kullanıcı Adı Hatalı!",
+    "api_register_wrong_utfusername_desc": "Lütfen Kullanıcı Adı Sadece Küçük Harf Girin! (utf-8 Geçersizdir.)",
+    "api_register_wrong_username_title": "Karakter Çok Veya Az!",
+    "api_register_wrong_username_desc": "Lütfen Kullanıcı Adı Doğru Girin!",
+    "api_register_wrong_nousername_title": "Bu Kullanıcı Adı Kullanılamaz!",
+    "api_register_wrong_nousername_desc": "Bu Kullanıcı Adı Yönetici Tarafından Yasaklanmıştır!",
+    "api_register_wrong_password_title": "Karakter Çok Veya Az!",
+    "api_register_wrong_password_desc": "Lütfen Şifre Doğru Girin!",
+    "api_register_wrong_repassword_title": "Tekrar Yanlış!",
+    "api_register_wrong_repassword_desc": "Lütfen Tekrar Şifre Doğru Girin!",
+
+    "api_register_already_email_title": "Email Zaten Kullanımda!",
+    "api_register_already_email_desc": "Girdiğiniz Email Kullanımdadır!",
+
+
+    "api_register_already_username_title": "Kullanıcı Adı Zaten Kullanımda!",
+    "api_register_already_username_desc": "Girdiğiniz Kullanıcı Adı Kullanımdadır!",
+
+    "api_register_error_mailsend_title": "Email Ulaşılamadı!",
+    "api_register_error_mailsend_desc": "Email Gönderilemedi!",
+    "api_register_success_title": "Kaydını Tamamla!",
+    "api_register_success_desc": "Email' inize Gelen Link İle Kaydını Tamamla! (0-6dk Sürebilir)",
+
+
+    // Api General
+    "api_loginreq_title": "Giriş Yapın!",
+    "api_loginreq_desc": "Etkileşim İçin Giriş Yapmanız Gerekir!",
+    "api_usernotfound_title": "Kullanıcı Bulunamadı",
+    "api_usernotfound_desc": "Sorun Oluştuğunu Düşünüyorsanız Bize Ulaşın!",
+    "api_anyfield": "Tüm Alanlar Doldurumalıdır!",
+
+    "api_noinfo_title": "İletişime Geç!",
+    "api_noinfo_desc": "Lütfen Bizimle İletişime Geçin!",
+    "api_accnotfound_title": "Hesap Bulunamadı!",
+    "api_accnotfound_desc": "Hesabınız Artık Yok Veya Hiç Var Olmadı!",
+
+
+
+    // Api Database
+    "api_database_title": "Ciddi Sorun!",
+    "api_database_desc": "Database Bağlanırken Sorun Oluştu!",
+
+    // General Words
+
+    "back_home_page": "Anasayfa' ya Geri Dön!",
+    "back_page": "Geri Dön",
+    "search_something": "Birşeyler Arayın..",
+    "buy": "Satın Al",
+    "fastlogin": "Hızlı Giriş",
+    "noreached": "Ulaşılamadı!",
+    "email_or_username": "Email / Kullanıcı Adı",
+    "register": "Kayıt Ol",
+    "forgetpassword": "Şifremi Unuttum",
+    "email": "Email",
+    "username": "Kullanıcı Adı",
+    "password": "Şifre",
+    "repassword": "Tekrar Şifre",
+    "conditions": "Şartlar Ve Koşullar",
+    "accepting": "Kabul Ediyorum",
+    "buy": "Satın Al",
+    "cancel": "İptal Et",
+    "order": "Sıra",
+    "title": "Başlık",
+    "status": "Durum",
+    "date": "Tarih",
+    "category": "Kategori",
+    "balance": "Bakiye",
+    "order2": "Sipariş",
+    "suggestion": "Öneri",
+    "problem": "Sorun",
+    "product": "Ürün",
+    "products": "Ürünler",
+    "price": "Fiyat",
+    "amount": "Miktar",
+    "institutional": "Kurumsal",
+    "shortcut": "Kısayol",
+    "software": "Yazılım",
+    "all": "Tüm",
+    "loading": "Yükleniyor..",
+    "mesagewait": "Mesaj..",
+    "pagenotfound": "Sayfa Bulunamadı!",
+    "contact": "İletişim"
+}
